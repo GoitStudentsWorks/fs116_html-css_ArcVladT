76b09b42fb530e687742e927bbcb5b173a7b9376
arcturus <1303huawei@gmail.com>
2025-02-24 13:24:25 +0200

    add link by id for buttons in header and menu

--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ document.getElementById('close-btn').addEventListener('click', function () {
 document.getElementById('open-btn').addEventListener('click', function () {
   document.getElementById('modal').classList.toggle('is-open');
 });
+
+document.getElementById('a-one').addEventListener('click', function () {
+  document.getElementById('modal').classList.toggle('is-open');
+});
+
+document.getElementById('a-two').addEventListener('click', function () {
+  document.getElementById('modal').classList.toggle('is-open');
+});
+
+document.getElementById('a-three').addEventListener('click', function () {
+  document.getElementById('modal').classList.toggle('is-open');
+});
