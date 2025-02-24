b680bfffb598897d0247e2fdd604312c9ef56e76
arcturus <1303huawei@gmail.com>
2025-02-24 12:33:11 +0200

    fix modal window

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// menu
+document.getElementById('close-btn').addEventListener('click', function () {
+  document.getElementById('modal').classList.toggle('is-open');
+});
+
+document.getElementById('open-btn').addEventListener('click', function () {
+  document.getElementById('modal').classList.toggle('is-open');
+});
